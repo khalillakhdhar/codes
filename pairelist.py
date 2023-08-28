1874f1d0ecc95cbdab32f8bc71fe99f1d8dabecb
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2023-08-28 17:31:08 +0100

    Update pairelist.py

--- pairelist.py
+++ pairelist.py
@@ -1,12 +1,12 @@
 def main():
-    n = int(input("Entrez le nombre d'entiers à saisir : "))
+    n = int(input("Entrez le nombre d'entiers à saisir : ")) # définir la taille de la liste
     
-    entiers = []
-    for i in range(n):
+    entiers = [] #initialisation de la liste
+    for i in range(n): #lecture de la liste
         entier = int(input(f"Entrez l'entier #{i+1} : "))
         entiers.append(entier)
     
-    entiers_pairs = [x for x in entiers if x % 2 == 0]
+    entiers_pairs = [x for x in entiers if x % 2 == 0] # la liste paires dans entiers
     
     print("Les entiers pairs sont :", entiers_pairs)
 
