dac9aa38f94851a5e44204efe126a65412007ea4
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2023-08-28 17:44:07 +0100

    Update dicto.py

--- dicto.py
+++ dicto.py
@@ -1,8 +1,12 @@
 p1={"nom":"Lakhdhar", "prenom":"Khalil", "age":33,"profession":"enseignant"}
 p2={"nom":"Lakhdhar", "prenom":"Adel", "age":40,"profession":"médecin"}
+#p1["tel"]="20999333"
 print(p1)
 personnes=[]
 personnes.append(p1)
 personnes.append(p2)
 for p in personnes:
     print(p["profession"])
+# définir un dictionnaire joueur et lisez un liste des joueur avec ( nomComplet, age , adresse) au clavier
+# créer et remplissez un tableau de joueur puis
+# afficher les joueur en indiquant selon leurs age >18 sénior <18 juniors
