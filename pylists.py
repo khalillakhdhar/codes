d75709290d6edaa09b5ac3427cbfe81d2d553c24
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2023-08-28 16:27:52 +0100

    Initial commit

---
+++ pylists.py
@@ -0,0 +1,14 @@
+datas=["khalil","lakhdhar",33] #liste
+datas2=["Ali","lakhdhar",66]
+datas.append("enseignant")
+print(datas)
+#print(datas[0])
+#print(datas[-3])
+datas=datas+datas2
+
+
+for i in datas:
+    print(i)
+
+#écrire un programme python permettant de lire 
+#des entiers seulement positifs au clavier et de les mettre dans une liste puis afficher les entiers paire uniquement
